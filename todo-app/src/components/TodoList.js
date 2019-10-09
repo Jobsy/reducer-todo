56d35abdd2021f0d64b23e2976ced480f6f1f6b5
Oluwajoba <oluwajobabello@gmail.com>
2019-10-09 07:15:37 +0100

    create TodoList.js / add functional component / TodoList.js / step 3

---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const TodoList = (props) => {
+
+  return (
+    <ul>
+      <li key={props.key} className={props.done ? "done" : "hidden"}>
+        {props.title}
+        <button onClick={props.onDeleteHandle}>
+          Delete
+                </button>
+       
+        <button onClick={props.onCompleteHandle}>
+          Complete
+                </button>
+      </li>
+    </ul>
+  )
+}
+
+
+
+export default TodoList;
+
